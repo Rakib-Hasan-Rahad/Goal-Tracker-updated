bf3c56fc408e2469d4e070a7aa0a8a47fbf5c1cb
Rahad08 <112348435+Rakib-Hasan-Rahad@users.noreply.github.com>
2023-08-15 11:55:16 +0600

    Add files via upload

---
+++ Database/migrations/2023_03_26_180844_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->integer('goal_id');
+            $table->string('status');
+            $table->string('Priority');
+            $table->string('level');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
